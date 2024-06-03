44907f06e5274909051ac966940840cc09c80381
Dustin Burke <dustin@datarobot.com>
2024-06-03 07:57:22 -0400

    use kong for cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/burkestar/drone-plugin-chronosphere-change-events
 
 go 1.22.3
+
+require github.com/alecthomas/kong v0.9.0 // indirect
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"github.com/alecthomas/kong"
+	"fmt"
+)
+
+type Context struct {
+	Debug bool
+}
+
+type PublishCmd struct {
+	Category string `arg:"" name:"category" help:"Event category." env:"PLUGIN_CATEGORY"`
+}
+func (p *PublishCmd) Run(ctx *Context) error {
+	fmt.Println("Publishing event with category", p.Category)
+	return nil
+}
+
+
+var CLI struct {
+	Debug bool `help:"Enable debug mode."`
+
+	Publish PublishCmd `cmd:"" help:"Publish change event."`
+}
 
 func main() {
-	fmt.Println("Hello there!!")
+	ctx := kong.Parse(&CLI)
+	err := ctx.Run(&Context{Debug: CLI.Debug})
+	ctx.FatalIfErrorf(err)
 }
