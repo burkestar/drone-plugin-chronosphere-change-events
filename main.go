c1e09bac0f5a79f7a7d4957caed706dc0775a0fc
Dustin Burke <dustin@datarobot.com>
2024-06-03 08:52:20 -0400

    chronosphere api to post change event, hardcoded payload

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/alecthomas/kong"
+	"bytes"
 	"fmt"
+	"net/http"
 )
 
 type Context struct {
@@ -10,10 +12,34 @@ type Context struct {
 }
 
 type PublishCmd struct {
-	Category string `arg:"" name:"category" help:"Event category." env:"PLUGIN_CATEGORY" enum:"alerts,broadcasts,chronosphere,deploys,feature_flags,infrastructure,third_party"`
+	Category string `arg:"" name:"category" help:"Event category." env:"CATEGORY,PLUGIN_CATEGORY" enum:"alerts,broadcasts,chronosphere,deploys,feature_flags,infrastructure,third_party"`
+	ChronosphereEventsAPI string `arg:"" name:"chronosphere_events_api" help:"URL for Chronosphere Events API like https://ADDRESS.chronosphere.io/api/v1/data/events" env:"CHRONOSPHERE_EVENTS_API,PLUGIN_CHRONOSPHERE_EVENTS_API"`
+	ChronosphereApiToken string `arg:"" name:"chronosphere_api_token" help:"API token for Chronosphere" env:"CHRONOSPHERE_API_TOKEN,PLUGIN_CHRONOSPHERE_API_TOKEN"`
 }
 func (p *PublishCmd) Run(ctx *Context) error {
 	fmt.Println("Publishing event with category", p.Category)
+
+	var jsonStr = []byte(`{"event": {"title": "Dustin test", "category": "deploys", "type": "deploy_start", "payload_object": {}, "labels": {}, "source": "local", "happened_at": "2024-06-03T12:42:00Z"}}`)
+
+	if ctx.Debug {
+		fmt.Println("API:", p.ChronosphereEventsAPI)
+	}
+
+	req, err := http.NewRequest("POST", p.ChronosphereEventsAPI, bytes.NewBuffer(jsonStr))
+	req.Header.Set("API-Token", p.ChronosphereApiToken)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	if err != nil {
+		if ctx.Debug {
+			fmt.Println(resp)
+		}
+		panic(err)
+	}
 	return nil
 }
 func (p *PublishCmd) Validate() error {
